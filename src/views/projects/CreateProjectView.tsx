1196fd15012137a63bc62d8b6ede51ed7568a3c3
dany99222 <102881074+dany99222@users.noreply.github.com>
2025-12-14 16:24:43 -0800

    Se creo la pagina dashboardproyectos y crear proyectos

--- src/views/projects/CreateProjectView.tsx
+++ src/views/projects/CreateProjectView.tsx
@@ -1,16 +1,19 @@
 import { Link } from "react-router-dom";
-export default function DashboardView() {
+
+export default function CreateProjectView() {
   return (
     <>
-      <h1 className="text-5xl font-black">Mis Proyectos </h1>
+      <h1 className="text-5xl font-black">Crear Proyecto </h1>
       <p className="text-2xl font-light text-gray-500 mt-5">
-        Administra tus Proyectos
+       Formulario para crear Proyecto
       </p>
       <nav className="my-5">
         <Link
           className="bg-green-600 hover:bg-green-700 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
-          to={"/projects/create"}
-        >Nuevo Proyecto</Link>
+          to={"/"}
+        >
+          Volver a Proyectos
+        </Link>
       </nav>
     </>
   );
