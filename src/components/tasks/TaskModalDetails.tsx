8f6bdff2be410ddd7dc3ccb28450f40f77db6b2f
dany99222 <102881074+dany99222@users.noreply.github.com>
2025-12-20 12:48:04 -0800

    Mostrando las tareas en un modal y validar que existan

---
+++ src/components/tasks/TaskModalDetails.tsx
@@ -0,0 +1,93 @@
+import { Fragment } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { Navigate, useLocation, useNavigate, useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { getTaskById } from "@/api/TaskAPI";
+import { toast } from "react-toastify";
+
+export default function TaskModalDetails() {
+  //Leemos el id del projecto
+  const params = useParams();
+  const projectId = params.projectId!;
+
+  //Para redireccionar al usuario y eliminar datos de consulta
+  const navigate = useNavigate();
+
+  // Leer el valor que ingresamos en la url de TaskCard de "ver tarea"
+  const location = useLocation();
+  //obtener el valor de view task
+  const queryParams = new URLSearchParams(location.search);
+  const taskId = queryParams.get("viewTask")!;
+
+  //Si taskId tiene algo mostramos el modal en caso contrario no
+  const show = taskId ? true : false;
+
+  //   consulta a la base de datos para mostrar los datos de la tarea
+  //todas las useQuery usan query key
+  const { data, isError, error } = useQuery({
+    queryKey: ["task", taskId],
+    queryFn: () => getTaskById({projectId, taskId}),
+    enabled: !!taskId, //se ejecuta solo si TaskId existe
+    retry: false  //solo hace una consulta y ya
+  });
+
+  if(isError){
+    toast.error(error.message, {toastId: 'error'})
+    return <Navigate to={`/projects/${projectId}`} />
+  }
+
+  return (
+    <>
+      <Transition appear show={show} as={Fragment}>
+        <Dialog
+          as="div"
+          className="relative z-10"
+          onClose={() => navigate(location.pathname, { replace: true })}
+        >
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black/60" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-4xl transform overflow-hidden rounded-2xl bg-white text-left align-middle shadow-xl transition-all p-16">
+                  <p className="text-sm text-slate-400">Agregada el: </p>
+                  <p className="text-sm text-slate-400">
+                    Última actualización:{" "}
+                  </p>
+                  <Dialog.Title
+                    as="h3"
+                    className="font-black text-4xl text-slate-600 my-5"
+                  >
+                    Titulo aquí
+                  </Dialog.Title>
+                  <p className="text-lg text-slate-500 mb-2">Descripción:</p>
+                  <div className="my-5 space-y-3">
+                    <label className="font-bold">Estado Actual:</label>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  );
+}
--- src/views/projects/ProjectDetailView.tsx
+++ src/views/projects/ProjectDetailView.tsx
@@ -3,6 +3,7 @@ import AddTaskModal from "@/components/tasks/AddTaskModal";
 
 import EditTaskData from "@/components/tasks/EditTaskData";
 import { TaskList } from "@/components/tasks/TaskList";
+import TaskModalDetails from "@/components/tasks/TaskModalDetails";
 import { useQuery } from "@tanstack/react-query";
 import { Navigate, useNavigate, useParams } from "react-router-dom";
 
@@ -45,6 +46,7 @@ export default function ProjectDetailView() {
         <TaskList tasks={data.tasks} />
         <AddTaskModal />
         <EditTaskData />
+        <TaskModalDetails />
       </>
     );
 }
