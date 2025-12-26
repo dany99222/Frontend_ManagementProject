c0aaac21c178af7088beb785a2d3ab3f9ea33d6b
dany99222 <102881074+dany99222@users.noreply.github.com>
2025-12-26 15:03:05 -0800

    Se agrego chakra componente para verificar el token

--- src/views/auth/ConfirmAccountView.tsx
+++ src/views/auth/ConfirmAccountView.tsx
@@ -1,34 +1,56 @@
-
 import { Link } from "react-router-dom";
+import { PinInput, PinInputField } from "@chakra-ui/pin-input";
+import { useState } from "react";
+import type { ConfirmToken } from "@/types/index";
 
 export default function ConfirmAccountView() {
+  // state que almacena el token
+  const [token, setToken] = useState<ConfirmToken["token"]>("");
 
+  //Antes de escribir en el state
+  const handleChange = (token: ConfirmToken["token"]) => {
+    setToken(token);
+  };
 
+  //   funcion que se ejecuta cuando se completa cada input
+  const handleCompleted = (token: ConfirmToken["token"]) => {
+    console.log(token)
+  };
   return (
     <>
       <h1 className="text-5xl font-black text-white">Confirma tu Cuenta</h1>
       <p className="text-2xl font-light text-white mt-5">
-        Ingresa el código que recibiste por{''}
-        <span className=" text-green-500 font-bold">  e-mail</span>
+        Ingresa el código que recibiste por{""}
+        <span className=" text-green-500 font-bold"> e-mail</span>
       </p>
-      <form
-        className="space-y-8 p-10 bg-white mt-10"
-      >
-        <label
-          className="font-normal text-2xl text-center block"
-        >Código de 6 dígitos</label>
-
+      <form className="space-y-8 p-10 bg-white mt-10">
+        <label className="font-normal text-2xl text-center block">
+          Código de 6 dígitos
+        </label>
+        <div className="flex justify-center gap-5">
+          <PinInput
+            value={token}
+            onChange={handleChange}
+            onComplete={handleCompleted}
+          >
+            <PinInputField className="text-center w-10 h-10 rounded-lg border-gray-300 border placeholder-white" />
+            <PinInputField className="text-center w-10 h-10 rounded-lg border-gray-300 border placeholder-white" />
+            <PinInputField className="text-center w-10 h-10 rounded-lg border-gray-300 border placeholder-white" />
+            <PinInputField className="text-center w-10 h-10 rounded-lg border-gray-300 border placeholder-white" />
+            <PinInputField className="text-center w-10 h-10 rounded-lg border-gray-300 border placeholder-white" />
+            <PinInputField className="text-center w-10 h-10 rounded-lg border-gray-300 border placeholder-white" />
+          </PinInput>
+        </div>
       </form>
 
       <nav className="mt-10 flex flex-col space-y-4">
         <Link
-          to='/auth/new-code'
+          to="/auth/new-code"
           className="text-center text-gray-300 font-normal"
         >
           Solicitar un nuevo Código
         </Link>
       </nav>
-
     </>
-  )
+  );
 }
