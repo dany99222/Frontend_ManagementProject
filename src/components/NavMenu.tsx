0cbd65e872c397e764783227502dbed042362bc8
dany99222 <102881074+dany99222@users.noreply.github.com>
2026-01-03 12:15:04 -0800

    Cerrar Sesion

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -3,12 +3,20 @@ import { Popover, Transition } from "@headlessui/react";
 import { Bars3Icon } from "@heroicons/react/20/solid";
 import { Link } from "react-router-dom";
 import type { User } from "../types";
+import { useQueryClient } from "@tanstack/react-query";
 
 type NavMenuProp = {
   name: User["name"];
 };
 
 export default function NavMenu({ name }: NavMenuProp) {
+  const queryClient = useQueryClient();
+
+  const logout = () => {
+    localStorage.removeItem("AUTH_TOKEN");
+    queryClient.invalidateQueries({ queryKey: ["user"] });
+  };
+
   return (
     <Popover className="relative">
       <Popover.Button className="inline-flex items-center gap-x-1 text-sm font-semibold leading-6 p-1 rounded-lg bg-blue-500 hover:bg-blue-400 transition-colors">
@@ -26,17 +34,19 @@ export default function NavMenu({ name }: NavMenuProp) {
       >
         <Popover.Panel className="absolute left-1/2 z-10 mt-5 flex w-screen lg:max-w-min -translate-x-1/2 lg:-translate-x-48">
           <div className="w-full lg:w-56 shrink rounded-xl bg-white p-4 text-sm font-semibold leading-6 text-gray-900 shadow-lg ring-1 ring-gray-900/5">
-            <p className="text-center">Hola: <span className="text-green-700 text-lg">{name}</span></p>
-            <Link to="/profile" className="block p-2 hover:text-purple-950">
+            <p className="text-center">
+              Hola: <span className="text-slate-600">{name}</span>
+            </p>
+            <Link to="/profile" className="block p-2 hover:text-purple-800">
               Mi Perfil
             </Link>
-            <Link to="/" className="block p-2 hover:text-purple-950">
+            <Link to="/" className="block p-2 hover:text-purple-800 ">
               Mis Proyectos
             </Link>
             <button
-              className="block p-2 hover:text-purple-950"
+              className="block w-full p-2 bg-red-500 transition-colors hover:bg-red-600 text-white uppercase"
               type="button"
-              onClick={() => {}}
+              onClick={logout}
             >
               Cerrar Sesión
             </button>
