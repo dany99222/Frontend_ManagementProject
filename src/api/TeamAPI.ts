d945ea701b52ff64d08a722bbd80b4b1be5f0698
dany99222 <102881074+dany99222@users.noreply.github.com>
2026-01-08 20:56:54 -0800

    Agregando el usuario al proyecto

--- src/api/TeamAPI.ts
+++ src/api/TeamAPI.ts
@@ -1,5 +1,5 @@
 import { isAxiosError } from "axios";
-import type { Project, TeamMemberForm } from "../types";
+import type { Project, TeamMember, TeamMemberForm } from "../types";
 import api from "@/lib/axios";
 
 export async function findUserByEmail({
@@ -12,6 +12,25 @@ export async function findUserByEmail({
   try {
     const url = `/projects/${projectId}/team/find`;
     const { data } = await api.post(url, formData);
+
+    return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+  }
+}
+
+export async function addUserToProject({
+  projectId,
+  id,
+}: {
+  projectId: Project["_id"];
+  id: TeamMember['_id'];
+}) {
+  try {
+    const url = `/projects/${projectId}/team`;
+    const { data } = await api.post(url, {id});
 
     return data;
   } catch (error) {
--- src/components/team/SearchResult.tsx
+++ src/components/team/SearchResult.tsx
@@ -1,17 +1,46 @@
+import { addUserToProject } from "@/api/TeamAPI";
 import type { TeamMember } from "@/types/index";
+import { useMutation } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
+import { toast } from "react-toastify";
 
 type SearchResultProps = {
   user: TeamMember;
 };
 
 export default function SearchResult({ user }: SearchResultProps) {
+  const params = useParams();
+  const projectId = params.projectId!;
+
+  const { mutate } = useMutation({
+    mutationFn: addUserToProject,
+    onError: (error) => {
+      toast.error(error.message);
+    },
+    onSuccess: (data) => {
+      toast.success(data);
+    },
+  });
+
+  const handleAddUserToProject = () => {
+    const data = {
+      projectId,
+      id: user._id,
+    };
+
+    mutate(data)
+  };
+
   return (
     <>
       <p className="mt-5 mb-5 text-center text-xl font-bold">Resultado: </p>
       <div className=" md:px-5 flex border md py-5 flex-col md:flex-row justify-between items-center">
         <p className="font-medium text-gray-800">{user.name}</p>
         <p className="font-medium text-gray-500">{user.email}</p>
-        <button className="text-green-600 hover:bg-green-200 mt-5 md:mt-0  px-10 py-3 font-bold cursor-pointer">
+        <button
+          onClick={handleAddUserToProject}
+          className="text-green-600 hover:bg-green-200 mt-5 md:mt-0  px-10 py-3 font-bold cursor-pointer"
+        >
           Agregar al proyecto
         </button>
       </div>
