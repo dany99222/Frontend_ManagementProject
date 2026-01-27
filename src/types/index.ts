78dc1c464335f31378c8afc7da47c22fc9cde4b8
dany99222 <102881074+dany99222@users.noreply.github.com>
2026-01-26 21:35:24 -0800

    Creando funcion para cambiar nuestro password

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,6 +4,7 @@ import { z } from "zod";
 export const authSchema = z.object({
   name: z.string(),
   email: z.string(),
+  current_password: z.string(),
   password: z.string(),
   password_confirmation: z.string(),
   token: z.string(),
@@ -18,7 +19,12 @@ export type UserRegistrationForm = Pick<
 export type ForgotPasswordForm = Pick<Auth, "email">;
 export type RequestConfirmationCodeForm = Pick<Auth, "email">;
 export type NewPasswordForm = Pick<Auth, "password" | "password_confirmation">;
+export type UpdateCurrentUserPasswordForm = Pick<
+  Auth,
+  "current_password" | "password" | "password_confirmation"
+>;
 export type ConfirmToken = Pick<Auth, "token">;
+
 // -----------------------------------------------------------------------
 // Users
 
